f678c426494b89c806b2bd1e18b0c519f03b39fb
kcr125 <purerin05@naver.com>
2024-04-19 17:26:00 +0900

    Add Python File [hw6.py]

---
+++ hw6.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+for i in range(1, 10):
+    for x in range(2, 6):
+        print(f'{x} x {i} = {x*i:2d}', end='\t')
+    print()
+    
+print("\n")
+
+for i in range(1, 10):
+    for x in range(6, 10):
+        print(f'{x} x {i} = {x*i:2d}', end='\t')
+    print()
+
+
+# In[ ]:
+
+
+
+
